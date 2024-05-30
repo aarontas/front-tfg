e4aa0babf83c885b4819215c596662d84fd29492
Aaron Sanchez <aaron.sanchez@docplanner.com>
2024-05-31 00:40:16 +0200

    clean

--- src/views/CityProfile.vue
+++ src/views/CityProfile.vue
@@ -1,34 +1,8 @@
 <template>
-    <main class="main" v-if="userStore.isLogged">
+    <main class="main">
         <cityItem :city="city"/>
     </main>
 </template>
 <script setup>
-import { ref } from "vue";
-import Apicity from "../composition/Api.js"
 import cityItem from "../components/CityItem.vue";
-import { watch } from "vue";
-import { useRouter, useRoute } from 'vue-router'
-const userStore = useStore();
-const router = useRouter()
-const route = useRoute()
-const city = ref()
-
-const findcity = async (cityId) => {
-    try {
-        city.value = await Apicity.findcity(cityId)
-    }
-    catch (error) {
-        console.error('Failed while try to find city: ' + error);
-    }
-}
-
-checkIsLoged(userStore.isLogged)
-if (userStore.isLogged) {
-    findcity(route.params.id)
-}
-
-watch(() => userStore.isLogged, (currentyLogged) => {
-    checkIsLoged(currentyLogged)
-})
 </script>
