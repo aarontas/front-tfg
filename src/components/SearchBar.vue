9b23f70b8c348b6f0c850460894eb04e1fad5b4a
Aaron Sanchez <aaron.sanchez@docplanner.com>
2024-05-31 00:10:07 +0200

    Remove search

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,7 +1,4 @@
 <template>
-    <div class="search-bar">
-      <input type="text" placeholder="Search City" v-model="search" />
-    </div>
     <div class="filter-bar">
       <div class="filter-bar__select" >
         <div >
@@ -37,32 +34,6 @@ watch(selectCity, () => {
 });
 </script>
 <style scoped>
-.search-bar {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10px 20px;
-  background-color: #f5f5f5;
-  border-bottom: 1px solid #e5e5e5;
-}
-.search-bar input {
-  width: 100%;
-  padding: 10px;
-  border: 1px solid #e5e5e5;
-  border-radius: 4px;
-  outline: none;
-}
-.search-bar button {
-  padding: 10px;
-  border: 1px solid #e5e5e5;
-  border-radius: 4px;
-  outline: none;
-  cursor: pointer;
-  min-width: 100px;
-}
-.search-bar .clear {
-  margin-right: 10px;
-}
 .search-bar .add {
   background-color: #4caf50;
   color: #fff;
